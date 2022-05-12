eec3ef930fd6defe43424027b1c17e02d083ba90
Yasmin Reis Tello <mimirt08@gmail.com>
2022-05-12 10:05:30 -0300

    Exercicio Variaveis - 4.1

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,11 @@
+const myName="yasmin"
+const birthCity="Rio de Janeiro"
+let birthYear=1991
+console.log(myName, birthCity, birthYear)
+
+birthYear=2030
+console.log(birthYear)
+
+//birthCity="Belo Horizonte"
+//console.log(birthCity)
+//Não é possivel reatribuir valor para const
