e5e32991da6e3971c1f9bcc6ed2c915c268a5e13
Yasmin Reis Tello <mimirt08@gmail.com>
2022-05-20 11:09:28 -0300

    ParteIII

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/ParteIII/script.js
@@ -0,0 +1,11 @@
+let pai = document.getElementById('pai')
+
+for(let index =pai.childNodes.length-1; index >= 0; index-=1) {
+const currentChildren = pai.childNodes[index]
+  if(currentChildren.id!== 'elementoOndeVoceEsta'){
+  currentChildren.remove()
+ }
+}  
+
+const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+segundoEUltimoFilhoDoFilho.remove();
