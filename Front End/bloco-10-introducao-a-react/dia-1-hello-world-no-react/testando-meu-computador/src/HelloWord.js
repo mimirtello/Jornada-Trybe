f843cc2f680eb5e47e0515de63839906ac6c9ca3
Yasmin Reis Tello <mimirt08@gmail.com>
2022-07-15 16:22:20 -0300

    Hello World - React

---
+++ Front End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/HelloWord.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React from 'react'
+
+class HelloWord extends React.Component {
+  render(){
+    return <span className="Hello">Olá, Mundo!</span>
+  }
+}
+
+export default HelloWord;
