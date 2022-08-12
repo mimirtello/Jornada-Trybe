90fb6182ce489b40f5122069303facafa4603d7c
Yasmin Reis Tello <mimirt08@gmail.com>
2022-08-12 15:07:30 -0300

    Exercicios

---
+++ Front End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios/api.test.js
@@ -0,0 +1,24 @@
+const service = require("./service");
+
+describe("testando a requisição", () => {
+  service.fetchDog = jest.fn();
+  afterEach(service.fetchDog.mockReset);
+
+  test("testando requisição caso a promise resolva", async () => {
+    service.fetchDog.mockResolvedValue("request success");
+
+    service.fetchDog();
+    expect(service.fetchDog).toHaveBeenCalled();
+    expect(service.fetchDog).toHaveBeenCalledTimes(1);
+    await expect(service.fetchDog()).resolves.toBe("request success");
+    expect(service.fetchDog).toHaveBeenCalledTimes(2);
+  });
+
+  test("testando requisição caso a promise seja rejeitada", async () => {
+    service.fetchDog.mockRejectedValue("request failed");
+
+    expect(service.fetchDog).toHaveBeenCalledTimes(0);
+    await expect(service.fetchDog()).rejects.toMatch("request failed");
+    expect(service.fetchDog).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ Front End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios/service.js
@@ -0,0 +1,26 @@
+const randomNumber = () => Math.floor(Math.random() * 101);
+
+//Exercicio 04 
+const firstFunction = (str) => str.toUpperCase();
+const secondFunction = (str) =>  str.charAt(0);
+const thirdFunction = (str1, str2) => str1.concat(str2);
+
+//Exercicio 05
+
+function fetchDog() {
+  return fetch("https://dog.ceo/api/breeds/image/random").then(response =>
+    response
+      .json()
+      .then(json =>
+        response.ok ? Promise.resolve(json) : Promise.reject(json)
+      )
+  );
+}
+
+module.exports = {
+  randomNumber ,
+  firstFunction,
+  secondFunction,
+  thirdFunction,
+  fetchDog,
+};
