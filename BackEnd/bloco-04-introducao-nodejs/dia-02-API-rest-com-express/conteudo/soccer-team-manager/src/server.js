cfb1cfcafcaff39beb416b26423ed51cb9f69f1d
Yasmin Reis Tello <mimirt08@gmail.com>
2022-10-28 15:35:12 -0300

    Conteudo bloco 4 dia 2

---
+++ BackEnd/bloco-04-introducao-nodejs/dia-02-API-rest-com-express/conteudo/soccer-team-manager/src/server.js
@@ -0,0 +1,8 @@
+// src/server.js
+const { app } = require('./app');
+
+app.listen(3001, () => console.log('server running on port 3001'));
+
+// app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+module.exports = app;
