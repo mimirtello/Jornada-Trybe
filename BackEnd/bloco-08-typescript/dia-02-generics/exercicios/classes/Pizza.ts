b8db6dbf0d2c4e6cdae90a2ba338b5987e43d0a5
Yasmin Reis Tello <mimirt08@gmail.com>
2023-01-11 14:22:49 -0300

    Exercicios generics

---
+++ BackEnd/bloco-08-typescript/dia-02-generics/exercicios/classes/Pizza.ts
@@ -0,0 +1,10 @@
+// ./Pizza.ts
+
+type Slices  = 4 | 6 | 8;
+
+interface Pizza {
+  flavor: string,
+  slices: Slices
+}
+
+export default Pizza
---
+++ BackEnd/bloco-08-typescript/dia-02-generics/exercicios/classes/PizzaFlavorsTypes.ts
@@ -0,0 +1,11 @@
+// ./PizzaFlavorsTypes.ts
+
+type Common = "Calabresa" | "Frango" | "Pepperoni";
+type Vegetarian = "Marguerita" | "Palmito" | "Cogumelos";
+type Sugar = "Nutela" | "Goiabada com Queijo" | "Marshmallow";
+
+export {
+  Common,
+  Vegetarian,
+  Sugar
+}
