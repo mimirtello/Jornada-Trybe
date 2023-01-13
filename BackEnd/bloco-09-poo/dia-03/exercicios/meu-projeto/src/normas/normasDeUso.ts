815d1b72b80fa1338e8191905485500da431f435
Yasmin Reis Tello <mimirt08@gmail.com>
2023-01-13 13:49:41 -0300

    Subindo exercicio dia 03 poo

---
+++ BackEnd/bloco-09-poo/dia-03/exercicios/meu-projeto/src/normas/normasDeUso.ts
@@ -0,0 +1,13 @@
+// src/normas/normasDeUso.ts
+
+const normas = {
+    tenis: {
+      piso: 'saibro',
+      dupla: true,
+    },
+    futebol: {
+      chuteira: 'trava',
+    },
+  };
+  
+  export default normas;
