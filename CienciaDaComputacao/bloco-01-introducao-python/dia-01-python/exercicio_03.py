5a44aa49d0e8f5f0fab260aa5e21e88e04b099f2
Yasmin Reis Tello <mimirt08@gmail.com>
2023-03-21 13:39:43 -0300

    Dia 01 - bloco01 - Ciencia da Computacao

---
+++ CienciaDaComputacao/bloco-01-introducao-python/dia-01-python/exercicio_03.py
@@ -0,0 +1,7 @@
+# Faça um programa que, dado um valor n qualquer,
+# tal que n > 1, imprima na tela um quadrado feito
+# de asteriscos de lado de tamanho n. Por exemplo:
+
+def draw_square(n):
+    for row in range(n):
+        print(n * '*')
